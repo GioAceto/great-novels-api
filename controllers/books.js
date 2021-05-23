ee2af2ba39d26d5a3c5cacc8df0f6b8e3b3c427e
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-23 14:02:48 -0400

    add to getAllBooks function so that it returns all books

--- controllers/books.js
+++ controllers/books.js
@@ -2,7 +2,11 @@
 const models = require('../models')
 
 const getAllBooks = async (req, res) => {
+  const books = await models.Books.findAll({
+    include: [{ model: models.Authors }, { model: models.Genres }]
+  })
 
+  return res.send(books)
 }
 
 const getBookById = async (req, res) => {
