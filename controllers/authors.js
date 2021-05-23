cad408a92c6cb30459f1d079061769a441e4641b
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-23 12:58:28 -0400

    fix models files - moved out of controller files into models

--- controllers/authors.js
+++
@@ -1,9 +0,0 @@
-const Authors = (connection, Sequelize) => {
-  return connection.define('authors', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    firstName: { type: Sequelize.STRING, allowNull: false },
-    lastName: { type: Sequelize.STRING, allowNull: false }
-  })
-}
-
-module.exports = Authors
--- controllers/books.js
+++ controllers/books.js
@@ -1,9 +1,1 @@
-const Books = (connection, Sequelize, Authors) => {
-  return connection.define('books', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    title: { type: Sequelize.STRING, allowNull: false },
-    authorId: { type: Sequelize.INTEGER, references: { model: Authors, key: 'id' } },
-  })
-}
 
-module.exports = Books
