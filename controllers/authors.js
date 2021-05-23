a8d93e6980142d01d13e4f8c5a1cb068d6037ddc
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-23 13:45:21 -0400

    add to getAllAuthors function so it returns all authors

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,7 +1,9 @@
 const models = require('../models')
 
 const getAllAuthors = async (req, res) => {
+  const authors = await models.Authors.findAll()
 
+  return res.send(authors)
 }
 
 const getAuthorById = async (req, res) => {
