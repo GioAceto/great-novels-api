1e192bf4f26d46e9af0661efa19ce21104839b9b
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-23 13:49:08 -0400

    add to getAuthorById function so that it returns the author with the provided id

--- controllers/authors.js
+++ controllers/authors.js
@@ -7,7 +7,18 @@ const getAllAuthors = async (req, res) => {
 }
 
 const getAuthorById = async (req, res) => {
+  const { id } = req.params
 
+  const author = await models.Authors.findOne({
+    where: { id },
+    include: [{
+      model: models.Books,
+      include: [{ model: models.Genres }]
+    }]
+  })
+
+  return author
+    ? res.send(author) : res.sendStatus(404)
 }
 
 module.exports = { getAllAuthors, getAuthorById }
