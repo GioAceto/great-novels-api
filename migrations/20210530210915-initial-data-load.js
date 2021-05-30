bd23ea3f00c35eec759b8b3949bf8e9b4b802ed0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-30 17:50:17 -0400

    add up function for bulk insert for authors

--- migrations/20210530210915-initial-data-load.js
+++ migrations/20210530210915-initial-data-load.js
@@ -1,21 +1,29 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface) => {
+
+    await queryInterface.bulkInsert('authors', [
+      { nameFirst: 'Bram', nameLast: 'Stoker' },
+      { nameFirst: 'Oscar', nameLast: 'Wilde' },
+      { nameFirst: 'Alice', nameLast: 'Walker' },
+      { nameFirst: 'Leo', nameLast: 'Tolstoy' },
+      { nameFirst: 'Charles', nameLast: 'Dickens' },
+      { nameFirst: 'Arthur', nameLast: 'Miller' },
+      { nameFirst: 'Alexandre', nameLast: 'Dumas' },
+      { nameFirst: 'Arthur Conan', nameLast: 'Doyle' },
+      { nameFirst: 'Robert Louis', nameLast: 'Stevenson' },
+      { nameFirst: 'Fyodor', nameLast: 'Dostoyevsky' },
+      { nameFirst: 'Agatha', nameLast: 'Christie' },
+      { nameFirst: 'Ray', nameLast: 'Bradbury' },
+      { nameFirst: 'George', nameLast: 'Orwell' },
+      { nameFirst: 'H.G.', nameLast: 'Wells' },
+      { nameFirst: 'Chinua', nameLast: 'Achebe' }
+    ])
+
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface) => {
+
   }
-};
+}
